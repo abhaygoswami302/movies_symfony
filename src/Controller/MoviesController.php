847a1f5e33b90f024ab02da448682b9ca90a2ddf
kaurlove12345 <lovepreet@nimbletechnocrats.com>
2022-03-15 12:00:02 +0530

    first symfony comit

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    /**
+     * @Route("/movies", name="app_movies")
+     */
+    public function index(): Response
+    {
+        $movies = ['Avengers:Endgame', 'Inception', 'Black Widow', 'Loki'];
+        
+        return $this->render('index.html.twig',[
+            'movies' => $movies
+        ]);
+    }
+
+}
